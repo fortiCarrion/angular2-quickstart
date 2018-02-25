64c59719709cc9c8e3ab3cf4bafa171e4e6d2d52
forticarrion <jaircarrion@hotmail.com>
2018-02-25 15:28:44 -0300

    contato service

--- app/contatos/contato-detalhe.component.ts
+++ app/contatos/contato-detalhe.component.ts
@@ -1,8 +1,35 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ActivatedRoute, Params} from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Contato } from './contato.model';
+import { ContatoService } from './contato.service'
 
 @Component({
     moduleId: module.id,
     selector:'contato-detalhe',
     templateUrl: 'contato-detalhe.component.html'
 })
-export class ContatoDetalheComponent{}
+export class ContatoDetalheComponent implements OnInit{
+
+    constructor(
+        private contatoService: ContatoService,
+        private route: ActivatedRoute,
+        private location: Location
+    ){}
+
+    ngOnInit(): void{
+        console.log("on init");
+        //params: retorna um observabel que contem no parametro da nossa rota
+        this.route.params.forEach((params: Params) => {
+            //+: converte para um numero a string
+            let id: number = +params['id'];
+            console.log(id);
+
+            this.contatoService.getContato(id)
+                .then((contato: Contato) => {
+                    console.log(contato);
+                });
+        });
+    }
+} 
--- app/contatos/contato.service.ts
+++ app/contatos/contato.service.ts
@@ -6,7 +6,23 @@ import {CONTATOS} from './contatos-mock';
 //emite meta-dados para o angular2, para identificar outras dependencias e fazer as insercoes das dependencias de forma correta
 @Injectable()
 export class ContatoService{
+    
     getContatos(): Promise<Contato[]>{
         return Promise.resolve(CONTATOS);
+    }
+
+    getContato(id: number): Promise<Contato>{
+        return this.getContatos()
+            .then((contatos: Contato[]) => {
+                return contatos.find((contato) =>{
+                    return contato.id === id;
+                });
+            });
+    }
+
+    getContatosSlowly(): Promise<Contato[]>{
+        return new Promise((resolve, reject) =>{
+            setTimeout(resolve, 3000);
+        }).then(() => this.getContatos());
     }
 }
